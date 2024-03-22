25507c00b9d25a9e9f7f15d2261f3e63773531f6
TimSon777 <dusalimov.tima@yandex.ru>
2024-03-22 15:55:07 +0300

    add console view and stemming and etc

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+try
+{
+    Log.Information("Start");
+
+    var pathToTexts = Path.Combine(Directory.GetCurrentDirectory(), "Texts", "labeledTrainData.tsv");
+    var pathToStopWords = Path.Combine(Directory.GetCurrentDirectory(), "StopWords", "stop-words.txt");
+    var pathToLemmitization = Path.Combine(Directory.GetCurrentDirectory(), "Lemmitization", "lemmitization.lem");
+    
+    await ApplicationRunner.RunAsync(pathToTexts, pathToStopWords, pathToLemmitization);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
